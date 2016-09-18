b6a8d1d613b6b12f77524f79b124a232fd720dfd
JFreyra <efreyra@stuy.edu>
2016-09-18 12:09:41 -0400

    fixed issues with prob1

--- Problem1.c
+++ Problem1.c
@@ -6,7 +6,7 @@ int main(){
   int iter;
   int counter = 0;
   for(iter = 0; iter < 1000; iter++){
-    if(iter/3 == 1 || iter/5 == 1)
+    if(iter%3 == 0 || iter%5 == 0)
       counter += iter;
   }
 
